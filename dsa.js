b1e76290100ca9e3e7aa017420762ba76b4b51c1
DEV-NG <nwachukujosh2000@gmail.com>
2025-01-16 22:45:33 +0100

    main

--- dsa.js
+++ dsa.js
@@ -1,11 +1,13 @@
-const groceries = ['milk','bread','donut','flour','sugar','butter',]
+const groceries = ['milk','bread','donut','flour','sugar','butter',];
 
 const searchForItem = (item) => {
-    for (let i =0; i < groceries.length; i++){
+    for (let i = 0; i < groceries.length; i++){
 
     
-        if (groceries[i]) {
-
+        if (groceries[i] == item)  {
+            console.log(`Found; ${item}`);
         }
     }
-}
+};
+
+searchForItem("bread")
